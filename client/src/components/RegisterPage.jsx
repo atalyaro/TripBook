d6a03be6e9cc97b66842f342984d2d9aef5e6ed4
atalyaro <68594132+atalyaro@users.noreply.github.com>
2020-12-18 17:27:54 +0200

    added react login and register forms with server

--- client/src/components/RegisterPage.jsx
+++ client/src/components/RegisterPage.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 export default function RegisterPage({ setloginOrSignin, loginOrSignin }) {
+    const [private_name, setprivate_name] = useState("")
+    const [family_name, setfamily_name] = useState("")
+    const [user_name, setuser_name] = useState("")
+    const [password, setpassword] = useState("")
+    const [error, seterror] = useState("")
 
+    const adduser = async () => {
+        const res = await fetch("http://localhost:1000/auth/register", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ private_name, family_name, user_name, password })
+        })
+        const data = await res.json()
+        if (data.err) {
+            seterror(data.msg)
+        } else {
+            seterror("you have signed in succesfully")
+            setTimeout(() => {
+                setloginOrSignin(!loginOrSignin)
+            }, 2000)
+        }
+    }
     return (
         <div>
-            <input type="text" placeholder="private name" />
-            <input type="text" placeholder="family name" />
-            <input type="text" placeholder="username" />
-            <input type="text" placeholder="password" />
-            <button onClick={() => setloginOrSignin(!loginOrSignin)}>Sign-up</button>
+            <input type="text" placeholder="private name" onChange={(e) => setprivate_name(e.target.value)} />
+            <input type="text" placeholder="family name" onChange={(e) => setfamily_name(e.target.value)} />
+            <input type="text" placeholder="username" onChange={(e) => setuser_name(e.target.value)} />
+            <input type="text" placeholder="password" onChange={(e) => setpassword(e.target.value)} />
+            <button onClick={adduser}>Sign-up</button>
+            <p>{error}</p>
         </div>
     )
 }
